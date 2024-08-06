221907e6268cf08bf2e5c7fb8a23e91632b8a721
Maxime Collombin <93529214+maxcollombin@users.noreply.github.com>
2024-08-06 14:25:25 +0200

    reorganise repo structure

--- antlr/src/main.py
+++ antlr/src/main.py
@@ -1,15 +1,23 @@
+import os
 import sys
 from parser import parse_input
 from converter import convert_to_sld, convert_to_qml
 
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: python main.py path_to_input_file path_to_output_file format(sld|qml)")
+    if len(sys.argv) != 3:
+        print("Usage: python main.py path_to_input_file path_to_output_file")
         sys.exit(1)
     
     input_file = sys.argv[1]
     output_file = sys.argv[2]
-    format_type = sys.argv[3].lower()
+    
+    # Extract the file extension and determine the format type
+    _, file_extension = os.path.splitext(output_file)
+    format_type = file_extension.lower().strip('.')
+
+    if format_type not in ['sld', 'qml']:
+        print("Error: Unsupported file format. Please use 'sld' or 'qml'.")
+        sys.exit(1)
     
     data = parse_input(input_file)
     
